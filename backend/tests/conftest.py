37aec437e05d771bc75fcd95c55e4fa0787f1309
triglan <121035322+triglan@users.noreply.github.com>
2026-03-20 21:43:05 +0900

    test용 환경변수 conftest.py 추가

---
+++ backend/tests/conftest.py
@@ -0,0 +1,8 @@
+import os
+
+
+os.environ["MYSQL_HOST"] = "test-host"
+os.environ["MYSQL_PORT"] = "4000"
+os.environ["MYSQL_DATABASE"] = "test-db"
+os.environ["MYSQL_USER"] = "test-user"
+os.environ["MYSQL_PASSWORD"] = "test-password"
